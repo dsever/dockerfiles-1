b851db969ec4128a1fe393df3cd53d020de33366
Dubravko Sever <dubravko.sever@pan-net.eu>
2020-07-07 20:32:37 +0200

    Finishing restore

--- postgres-restore-s3/restore.sh
+++ postgres-restore-s3/restore.sh
@@ -50,36 +50,52 @@ export AWS_DEFAULT_REGION=$S3_REGION
 
 export PGPASSWORD=$POSTGRES_PASSWORD
 POSTGRES_HOST_OPTS="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER"
-export PIT=${RESTORE_TO^^}
+
+export PIT=$(echo ${RESTORE_TO} | tr  '[:lower:]' '[:upper:]' )
 
 echo "Finding latest backup"
 
+generateAWSPath(){
 
-if [ "$AES_KEY"  == "**None**" ]; then
- LATEST_BACKUP=$(aws s3 ls s3://$S3_BUCKET/$S3_PREFIX/ | sort | tail -n 1 | awk '{ print $4 }')
- echo "Fetching ${LATEST_BACKUP} from S3"
- aws s3 cp s3://$S3_BUCKET/$S3_PREFIX/${LATEST_BACKUP} dump.sql.gz
- gzip -d dump.sql.gz
+  if [ "$RESTORE_FILE" != "**None**" ]; then
+    aws s3api head-object --bucket $S3_BUCKET --key $RESTORE_FILE || not_exist=true
+    if [ $not_exist ]; then
+       echo "Wrong filename or file doesn't exist"
+       exit 1
+    else
+       BACKUP=$(aws s3 ls s3://$S3_BUCKET/$RESTORE_FILE | tail -n 1 ${AES_FILTER} | awk '{print $4}')
 
-###PIT implementation is missing
+    fi
 
-else
-  LATEST_BACKUP=$(aws s3 ls s3://$S3_BUCKET/$S3_PREFIX/ --recursive  | tail -n 1 | awk '{print $4}')
-  echo "Retriving backup  $LATEST_BACKUP"
-  aws s3 cp s3://$S3_BUCKET/${LATEST_BACKUP} dump.sql.gz.dat
-  echo "Fetching ${LATEST_BACKUP} from S3"
-  ls -la
-  openssl enc -in dump.sql.gz.dat  -out dump.sql.gz -d -aes256 -md sha256 -pbkdf2 -k $AES_KEY
-  gzip -d dump.sql.gz
+  elif [ "$PIT" = "LATEST" ]; then
+    if [ "$AES_KEY" = "**None**" ]; then
+      BACKUP=$(aws s3 ls s3://$S3_BUCKET/$S3_PREFIX/ --recursive  | tail -n 1 | awk '{print $4}')
+    else
+      BACKUP=$(aws s3 ls s3://$S3_BUCKET/$S3_PREFIX/ --recursive  | tail -n 1| grep dat | awk '{print $4}')
+    fi
+  else
+    echo "Can be absolute path or LATEST"
 fi
 
+}
+
+echo "Fetching ${BACKUP} from S3"
+
+if [ "AES_KEY" != "**None**" ]; then
+  aws s3 cp s3://$S3_BUCKET/${BACKUP} dump.sql.gz.dat
+  openssl enc -in dump.sql.gz.dat  -out dump.sql.gz -d -aes256 -md sha256 -pbkdf2 -k $AES_KEY
+else
+  aws s3 cp s3://$S3_BUCKET/${BACKUP} dump.sql.gz
+fi
+
+gzip -d dump.sql.gz
 
 if [ "${DROP_PUBLIC}" == "yes" ]; then
 	echo "Recreating the public schema"
 	psql $POSTGRES_HOST_OPTS -d $POSTGRES_DATABASE -c "drop schema public cascade; create schema public;"
 fi
 
-echo "Restoring ${LATEST_BACKUP}"
+echo "Restoring ${BACKUP}"
 
 #psql $POSTGRES_HOST_OPTS -d $POSTGRES_DATABASE < dump.sql
 psql $POSTGRES_HOST_OPTS -d $POSTGRES_DATABASE < dump.sql
